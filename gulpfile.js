9178ca5ff669bfb0e04fd6194785cfc10aac985f
Lorenc Estrefi <jlorenc1986@gmail.com>
2015-06-25 17:07:32 +0200

    adds less

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,22 @@ var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 var react = require('gulp-react');
 var nodemon = require('gulp-nodemon');
-
+var less    = require('gulp-less');
+var path    = require('path');
 
 gulp.task('react', function () {
     return gulp.src('./ui_components/*.jsx').pipe(react()).pipe(gulp.dest('./public/javascripts/components/'));
 });
 
+
+gulp.task('less', function(){
+    return gulp.src('./styles/**/*.less')
+        .pipe(less({
+
+        }))
+        .pipe(gulp.dest('./public/stylesheets/'));
+
+});
 
 gulp.task('watch', function(){
     gulp.watch('./ui_components/**/*.jsx',['react']);
