76a560a7c1c923676fe4297316a49a9a0834337d
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-02-25 23:03:11 +0100

    adds navbar component

---
+++ ui_components/Navbar.js
@@ -0,0 +1,32 @@
+/* @jsx REACT.DOM */
+
+var Navbar = React.createClass({
+
+        getInitialState: function() {
+
+
+
+
+        },
+
+        componentDidlMount: function() {
+
+
+
+        },
+
+
+        render: function() {
+
+
+            return <div className="">
+
+
+
+            </div>
+        }
+    }),
+
+    parent = document.getElementById("viewContainer");
+
+React.render(<ListOfBoxes url="http://127.0.0.1:3000/offers/list" />, parent);
