5d75d921aabfd110751657861a6e7be15994c756
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-03-01 21:06:59 +0100

    refactor navbar

--- public/javascripts/components/Navbar.js
+++ public/javascripts/components/Navbar.js
@@ -2,11 +2,41 @@
 
 var Navbar = React.createClass({displayName: "Navbar",
 
+  componentDidMount: function() {
+    console.log("navbar did mount" );
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+
+  },
+
+  componentWillMount: function() {
+
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+    
+  },
+
+
         render: function() {
 
 
-            return React.createElement("nav", null, 
-            "navbar"
+            return React.createElement("header", null, 
+              React.createElement("nav", null, 
+
+                React.createElement("ul", {className: "ls-navbar"}, 
+                  React.createElement("li", {className: "ls-navbar__item ls-navbar__item--home"}, " ", React.createElement("a", {class: "ls-button ls-button--primary", href: "/"}, "home")), 
+                  React.createElement("li", {className: "ls-navbar__item"}), 
+                  React.createElement("li", {className: "ls-navbar__item ls-navbar__item--signin"}, 
+
+            				 React.createElement("a", {className: "ls-button ls-button--primary", href: "/login"}, "login "), 
+
+            				React.createElement("a", {className: "ls-button ls-button--primary", href: ""}, "SignOut")
+
+            			)
+                )
+              )
             )
         }
     });
--- ui_components/Navbar.jsx
+++ ui_components/Navbar.jsx
@@ -2,12 +2,42 @@
 
 var Navbar = React.createClass({
 
+  componentDidMount: function() {
+    console.log("navbar did mount" );
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+
+  },
+
+  componentWillMount: function() {
+
+  },
+
+  componentWillUpdate: function(nextProps, nextState) {
+
+  },
+
+ 
         render: function() {
 
 
-            return <nav>
-            navbar
-            </nav>
+            return <header>
+              <nav>
+
+                <ul className="ls-navbar">
+                  <li className="ls-navbar__item ls-navbar__item--home"> <a class="ls-button ls-button--primary"  href="/">home</a></li>
+                  <li className="ls-navbar__item"></li>
+                  <li className="ls-navbar__item ls-navbar__item--signin">
+
+            				 <a className="ls-button ls-button--primary" href="/login">login </a>
+
+            				<a className="ls-button ls-button--primary"  href="">SignOut</a>
+
+            			</li>
+                </ul>
+              </nav>
+            </header>
         }
     });
 
