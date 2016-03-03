2ff4ae73068b4550fcfde33aae29ed59adffe547
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-03-03 23:14:03 +0100

    wip login

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var jwt = require('jsonwebtoken');
+var url = require('url');
 var router = express.Router();
+var bodyParser = require('body-parser');
 
 /* auth */
 router.post('/', function(req, res) {
   var token = jwt.sign({ tok: 'lsToken'}, 'fkdlfkdslfk');
+  var username = req.query['username'];
 
-	res.render('platform/',{ title: "platform",client_token: token, user:{ name: 'lorenc'}});
+	res.render('platform/',{ title: "platform",client_token: token, user:{name: username}});
 
 });
 
