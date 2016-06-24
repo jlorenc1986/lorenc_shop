f097d16fa6b3d83a9fd0dabeab78b8fc585606bd
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-06-25 00:16:04 +0200

    improves eslint configuration
    oportunistic refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,44 @@
-var gulp  =  require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var watch = require('gulp-watch');
+var gulp = require('gulp');
 var react = require('gulp-react');
 var nodemon = require('gulp-nodemon');
-var sass    = require('gulp-sass');
-var path    = require('path');
+var sass = require('gulp-sass');
 var sassLint = require('gulp-scss-lint');
 var livereload = require('gulp-livereload');
 var notify = require('gulp-notify');
+var eslint = require('gulp-eslint');
 
 gulp.task('react', function () {
+    'use strict';
     return gulp.src('./ui_components/*.jsx').pipe(react()).pipe(gulp.dest('./public/javascripts/components/'));
 });
 
-gulp.task('lint', function(){
+gulp.task('lint-sass', function () {
 
   return gulp.src('/styles/**/*.scss')
     .pipe(sassLint());
 
 });
 
-gulp.task('sass', function(){
-    return gulp.src('./styles/**/*.scss')
-        .pipe(sass({
+gulp.task('lint-sass', function () {
 
-        }))
-        .pipe(gulp.dest('./public/stylesheets/'));
+  return gulp.src('/js/**/*.scss')
+    .pipe(sass());
 
 });
 
-gulp.task('watch', function(){
+gulp.task('lint-js', function () {
+    return gulp.src('./ui_components/**/*.jsx')
+        .pipe(react())
+        .pipe(eslint());
+});
+
+gulp.task('watch', function () {
     gulp.watch('./ui_components/**/*.jsx',['react']);
-    gulp.watch('./styles/**/*.scss',['sass']);
+    gulp.watch('./styles/**/*.scss',['lint-sass']);
 });
 
 
-gulp.task('develop', function(){
+gulp.task('develop', function () {
     nodemon({
       script: 'app.js',
       ext: 'html js jsx ejs scss',
@@ -44,11 +47,11 @@ gulp.task('develop', function(){
         .on('restart', function () {
 
               gulp.src('app.js')
-			           .pipe(livereload())
-			           .pipe(notify('Reloading page, please wait...'));
+                .pipe(livereload())
+			    .pipe(notify('Reloading page, please wait...'));
 
 
         });
 });
 
-gulp.task('default', ['develop','react', 'watch']);
+gulp.task('default', ['develop', 'react', 'watch']);
