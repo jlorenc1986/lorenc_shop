199c0e1ea60e57eb1d1737955db4b673c7ca4cc9
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-06-26 17:55:25 +0200

    adds tests

--- test/routes.specs.js
+++ test/routes.specs.js
@@ -1,19 +1,21 @@
-var should = require('should');
-var request = require('supertest');
 var assert = require('assert');
-var express = require('express');
-
-
 
 describe('routes SPECS', function () {
 
     it('dummy test', function () {
 
+        assert.equal(true, true, 'values are equal');
+ 
+    });
+
+
+    it('dummy test 2 ', function () {
 
         assert.equal(true, true, 'values are equal');
-
         
     });
+
+
 });
 
 
