5569133abdb7b27da583c55cae1111dbead46aff
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-07-04 19:23:59 +0200

    removes auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,13 @@
 var express = require('express');
-var jwt = require('jsonwebtoken');
 var url = require('url');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
 /* auth */
 router.post('/', function(req, res) {
-  var token = jwt.sign({ tok: 'lsToken'}, 'fkdlfkdslfk');
   var username = req.query['username'];
-    console.log(req.body);
 
-    res.render('platform/', {title: "platform", client_token: token, user: {"username": req.body.username}});
+    res.render('platform/', {title: "platform", client_token: 'token', user: {"username": req.body.username}});
 
 });
 
