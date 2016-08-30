f8efbd3fbb0ac91c3d1799b2df713e5ce43b4cfd
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-08-30 22:06:42 +0200

    adds basic profile page

---
+++ client/apps/profile/profileAppView.js
@@ -0,0 +1,43 @@
+define(function (require, exports, module){
+
+
+	var RootView,
+		$ = require('jquery'),
+		_ = require('underscore'),
+		Backbone = require('backbone'),
+		Template = require('text!profileAppTemplate'),
+    ProfileView = require('profileView'),
+		Marionette = require('marionette');
+
+ProfileAppView = Marionette.LayoutView.extend({
+
+	el: '.ls-content-child',
+	template:  _.template(Template),
+
+  regions: {
+    toolbar: '.ls-profileAppToobar',
+    productContent: '.ls-profileAppContent'
+
+  },
+
+	initialize: function (opts) {
+
+			console.log('init ProfileAppView');
+	},
+
+	render: function () {
+    var _this = this;
+    _this.$el.html(_this.template);
+  },
+
+  onBeforeShow: function () {
+      this.showChildView('productContent', new ProfileView());
+  }
+
+
+
+
+	});
+
+	module.exports = ProductsAppView;
+});
