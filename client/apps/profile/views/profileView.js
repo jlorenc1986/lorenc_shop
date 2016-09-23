da1b852c82a47669bd0e96bfc0025ed550be6e93
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-09-23 19:17:18 +0200

    adds basic dynamic profile model

--- client/apps/profile/views/profileView.js
+++ client/apps/profile/views/profileView.js
@@ -6,6 +6,7 @@ define(function (require, exports, module){
 		_ = require('underscore'),
 		Backbone = require('backbone'),
 		Template = require('text!profileAppTemplate'),
+		User = require('UserModel'),
 		Marionette = require('marionette');
 
 ProfileView = Marionette.ItemView.extend({
@@ -16,12 +17,23 @@ ProfileView = Marionette.ItemView.extend({
 	initialize: function (opts) {
 
 			console.log('init ProfileView');
+			this.model = new User();
 	},
 
-	render: function () {
-    var _this = this;
-    _this.$el.html(_this.template);
-  }
+	serializeData: function () {
+		console.log('serializeData ', this.model );
+		return {
+					 name: this.model.get('name'),
+					 photoUrl: this.model.get('photoUrl'),
+					 nationality: this.model.get('nationality'),
+					 surname: this.model.get('surname')
+			 };
+	},
+
+	// onRender: function () {
+  //   var _this = this;
+  //   _this.$el.html(_this.template);
+  // }
 	});
 
 	module.exports = ProfileView;
--- client/models/User.js
+++ client/models/User.js
@@ -8,6 +8,14 @@ define(function (require, exports, module){
 
 	User = Backbone.Model.extend({
 
+		defaults: {
+			name: 'foo',
+			surname: 'foo',
+			photoUrl: 'urlPhoto',
+			nationality: 'nationality',
+			email: 'test@test.com'
+		},
+
 		initialize: function (opts) {
 			console.log('init user model');
 		}
