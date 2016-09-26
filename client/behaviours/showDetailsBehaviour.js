39864d8ca5255725ba102d9e0ae91afe78729286
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-09-26 22:59:58 +0200

    adds basic marionette behaviour

---
+++ client/behaviours/showDetailsBehaviour.js
@@ -0,0 +1,12 @@
+define(function (require, exports, module){
+    var ShowDetailsBehaviour,
+        $ = require('jquery'),
+        _ = require('underscore'),
+        Backbone = require('backbone'),
+        Marionette = require('marionette');
+
+    ShowDetailsBehaviour = Marionette.Behaviour.extend({
+
+    });
+    module.exports = ShowDetailsBehaviour;
+});
