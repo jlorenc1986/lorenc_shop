509cbd3e2072450edbee9d858f905ea207bd43f9
Lorenc Estrefi <jlorenc1986@gmail.com>
2016-10-05 11:50:15 +0200

    adds basics types

--- client/main.js
+++ client/main.js
@@ -1,55 +1,19 @@
-require.config({
-    paths: {
-
-        'jquery': '../node_modules/jquery/dist/jquery.min',
-        'tooltipster': '../node_modules/tooltipster/dist/js/tooltipster.bundle.min',
-        'underscore': '../node_modules/underscore/underscore-min',
-        'text': '../node_modules/text/text',
-        'bootstrap': '../node_modules/bootstrap/dist/js/bootstrap.min',
-        'backbone': '../node_modules/backbone/backbone-min',
-        'marionette': '../node_modules/backbone.marionette/lib/backbone.marionette.min',
-        'rootView': '../client/application/rootView',
-        'navbarTemplate': '../client/apps/navbar/templates/navbar-template.html',
-        'navbarView': '../client/apps/navbar/navbarView',
-        'homeTemplate': '../client/apps/home/templates/home-template.html',
-        'homeView': '../client/apps/home/homeView',
-        'productTemplate': '../client/apps/products/templates/product-template.html',
-        'productView': '../client/apps/products/views/productView',
-        'productModel': '../client/apps/products/models/productModel',
-        'productsCollection': '../client/apps/products/collections/productsCollection',
-        'productsListTemplate': '../client/apps/products/templates/productsList-template.html',
-        'productsListView': '../client/apps/products/views/productsListView',
-        'productsAppTemplate': '../client/apps/products/templates/productsApp-template.html',
-        'productsAppView': '../client/apps/products/productsAppView',
-        'profileAppTemplate': '../client/apps/profile/templates/profileApp-template.html',
-        'profileView': '../client/apps/profile/views/profileAppView',
-        'profileAppView': '../client/apps/profile/views/profileView',
-        'RouteController': '../client/application/routeController',
-        'UserModel': '../client/models/User',
-        'router': '../client/application/router',
-        'ShowDetailsBehaviour': '../client/behaviors/showDetailsBehavior',
-        'app': '../client/application/app'
-
-    },
-
-    shim: {
-        backbone: {
-            deps: ['underscore', 'jquery'],
-            exports:function () {
-                return Backbone.noConflict();
-            },
-            marionette: {
-                deps:['backbone'],
-                exports: 'Marionette'
-            }
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+define(["require", "exports", "backbone.marionette"], function (require, exports, Marionette) {
+    "use strict";
+    var App = (function (_super) {
+        __extends(App, _super);
+        function App() {
+            _super.apply(this, arguments);
         }
-    }
-});
-
-
-require(['app'], function(App){
+        return App;
+    }(Marionette.Application));
+    exports.App = App;
     var app = new App();
-    app.start();
-
-
+    app.start;
 });
+//# sourceMappingURL=main.js.map
---
+++ client/main.ts
@@ -0,0 +1,11 @@
+import * as $ from "jquery";
+import * as _ from "underscore";
+import * as Backbone from "backbone";
+import * as Marionette from "backbone.marionette";
+export class App extends Marionette.Application {
+
+}
+
+const app = new App();
+
+app.start;
