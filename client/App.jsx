7131f19acdeb21a83ec5659aaa69e75511ae177f
Lorenc Estrefi <jlorenc1986@gmail.com>
2017-11-23 02:33:59 +0100

    adapt eslint configuration

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import styles from './App.css';
-import Offer from '../ui_components/Offer';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
-  render() {
-    return (
-      <div className={styles.app}>
-        lorenc shop
-        <Offer id="1" />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className={styles.app} >
+      lorencShop
+    </div>
+  );
+};
+
+export default App;
