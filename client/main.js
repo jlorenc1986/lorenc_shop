509cb73e5689a4b57bb131634a4a67d319147b4a
Lorenc Estrefi <jlorenc1986@gmail.com>
2018-04-07 23:43:30 +0200

    fix store issue

--- client/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from '../redux/containers/Root.dev';
-
-ReactDOM.render(<Root />, document.getElementById('root'));
--- redux/containers/Root.dev.jsx
+++ redux/containers/Root.dev.jsx
@@ -1,14 +1,12 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
 import App from '../../client/App';
-import DevTools from './DevTools';
 import store from '../store';
 
 const Root = () => (
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>
+  </Provider>
 );
 
 export default Root;
